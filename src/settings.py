0d855b03f8c0c792c410dc50e22e0b1163876354
jjovalle99 <jj.ovalle@uniandes.edu.co>
2024-08-29 17:29:44 -0500

    chore: Add news_api to settings.py

--- src/settings.py
+++ src/settings.py
@@ -4,5 +4,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     samba_api_key: str
     samba_url: str
+    news_api: str
 
     model_config = SettingsConfigDict(env_file=".env")
