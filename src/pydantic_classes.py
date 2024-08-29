fc9595a3a779dec05060e08bf77990320ea72a7d
jjovalle99 <jj.ovalle@uniandes.edu.co>
2024-08-28 19:37:58 -0500

    Add Pydantic Metadata class for handling metadata information

---
+++ src/pydantic_classes.py
@@ -0,0 +1,11 @@
+from typing import Any
+from pydantic import BaseModel, Field
+
+
+class Metadata(BaseModel):
+    id: str
+    created: int
+    model: str
+    system_fingerprint: str
+    usage: dict[str, Any]
+    finish_reason: str = Field(default="unknown")
